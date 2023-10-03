37d87117a97a665e0ac6c67eca78007df76f687f
ignacior <ignacioruedaboada@hotmail.com>
2023-10-03 16:46:52 -0500

    feat: use better styles

--- index.php
+++ index.php
@@ -1,16 +1,28 @@
 <?php
 
+$HTML_FILENAME = 'print-content.html';
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['html'])) {
     $html = $_POST['html'];
     
-    $html .= file_get_contents('print-content.html');
+    $html .= file_get_contents($HTML_FILENAME);
 
-    file_put_contents('print-content.html', str_replace("javascript:window.print()", "", $html));
+    $html = str_replace("javascript:window.print()", "", $html);
+    $html = str_replace("<body", '<body style="display: flex; flex-direction: column; align-items: center;" ', $html);
+    $html = str_replace('class="esc-receipt"', 'class="esc-receipt" style="border-bottom: 3px solid red;" ', $html);
+
+    file_put_contents($HTML_FILENAME, $html);
     
     http_response_code(200);
     echo "HTML actualizado correctamente.";
 } else {
-    $html = file_get_contents('print-content.html');
+    $html = '';
+
+    if (isset($_GET['clear'])) {
+        file_put_contents($HTML_FILENAME, '');
+    } else {
+        $html = file_get_contents($HTML_FILENAME);
+    }
 
     header("Content-Type: text/html");
     echo $html;
